fe699dea81fac328f42712880c4285f7e33df2c9
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-11 18:14:54 +0100

    add book model with Django Admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,6 +6,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
 
-    serach_fields = ('title', 'author')
+    search_fields = ('title', 'author')
 
     list_filter = ('publication_year',)
