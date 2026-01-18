0797b0fbddbfd89c082a63c77939234a3ac4210d
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-18 16:18:50 +0100

    Add auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register_view
+from . import views
 
 urlpatterns = [
-    path("", list_books, name="list_books"),
-    path("books/", list_books, name="books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("", views.list_books, name="list_books"),
+    path("books/", views.list_books, name="books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication URLs
     path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register_view, name="register"),
+    path("register/", views.register, name="register"),   # <-- MUST be views.register
 ]
