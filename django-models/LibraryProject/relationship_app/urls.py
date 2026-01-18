970554d8376455dc06ce792d6c5d990dd643e5a8
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-18 17:48:37 +0100

    add task 4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("member-view/", views.member_view, name="member_view"),
 
 
-    path("book/add/", views.add_book, name="add_book"),
-    path("book/edit/<int:pk>/", views.edit_book, name="edit_book"),
-    path("book/delete/<int:pk>/", views.delete_book, name="delete_book"),
+   path("add_book/", views.add_book, name="add_book"),
+   path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+   path("book/delete/<int:pk>/", views.delete_book, name="delete_book"),
 ]
