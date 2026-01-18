569c840c59feff1d8ce0be26e95265400d3a5bea
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-18 15:19:48 +0100

    Add relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
 from .models import Library, Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
