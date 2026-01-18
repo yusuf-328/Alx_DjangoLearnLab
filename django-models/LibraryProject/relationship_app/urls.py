50469d6f9bdb47eaae4281af80be34c33c0504f1
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-18 15:21:16 +0100

    Add relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .views import list_books 
 
 urlpatterns = [
     path('', views.list_books, name='book_list'),                # <-- THIS MAKES /relationship/ WORK
