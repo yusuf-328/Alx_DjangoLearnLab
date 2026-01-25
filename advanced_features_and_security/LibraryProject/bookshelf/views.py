4ee92da2962b70b414e47058c79e630b6afed6b5
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-25 19:31:57 +0100

    add task 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 @login_required
 def book_list(request):
