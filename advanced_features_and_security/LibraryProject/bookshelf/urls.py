15daac672b29bf7bb92cfde9e6a6e0a485aa2fd9
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-25 18:10:25 +0100

    add Django permission and group setup

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_books, name='list_books'),
+    path('', views.book_list, name='book_list'),
     path('create/', views.create_book, name='create_book'),
     path('edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete/<int:pk>/', views.delete_book, name='delete_book'),
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,7 +5,7 @@ from .forms import BookForm
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
 
@@ -15,7 +15,7 @@ def create_book(request):
     form = BookForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('list_books')
+        return redirect('book_list')  # <-- fixed
     return render(request, 'bookshelf/create_book.html', {'form': form})
 
 
@@ -25,7 +25,7 @@ def edit_book(request, pk):
     form = BookForm(request.POST or None, instance=book)
     if form.is_valid():
         form.save()
-        return redirect('list_books')
+        return redirect('book_list')  # <-- fixed
     return render(request, 'bookshelf/edit_book.html', {'form': form})
 
 
@@ -34,5 +34,5 @@ def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
         book.delete()
-        return redirect('list_books')
+        return redirect('book_list')  # <-- fixed
     return render(request, 'bookshelf/delete_book.html', {'book': book})
