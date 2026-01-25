a9df7224ad72d4794354b87a09f2fccf94d4c3dd
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-01-25 19:28:34 +0100

    add task 2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,4 +4,7 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'publication_year']
+        fields = "__all__"
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
