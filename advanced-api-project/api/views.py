806d988b3953152c7bcbf3e164ab3fc5a0b5fe9b
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-02-02 22:41:34 +0100

    Filter, serach, and order implementation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
