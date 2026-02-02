787d7fb1b84023215797db54876e9739d0199d3b
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-02-02 21:46:34 +0100

    add task1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
