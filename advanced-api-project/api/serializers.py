9fb1e75e4e1bf9a3ff7346c1be7f3d3a215fa8e3
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-02-02 21:10:03 +0100

    add custom serializer

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import date
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                "Publication year cannot be in the future."
+            )
+        return value
+
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
