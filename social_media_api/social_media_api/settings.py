ed4ded6e5ab2bd5193da924e00b233424f8666ac
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-02-17 23:19:57 +0100

    Configure databasevfor production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
