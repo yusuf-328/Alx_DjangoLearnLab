f944ce379714484bc3482c6a18e1040769519672
Yusuf Edun Olashile <olashileyusuf3@gmail.com>
2026-02-17 20:28:07 +0100

    serializers.py updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
